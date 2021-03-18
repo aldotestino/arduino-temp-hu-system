b40eeac82c4664eeac1dc29c0bebbc18e4a9d2fa
aldotestino <aldo.testino@libero.it>
2021-03-18 20:34:12 +0100

    :sparkles: updated the arduino code

--- server/TempAndHu.ino
+++ server/TempAndHu.ino
@@ -1,29 +1,22 @@
+#include <Arduino.h>
 #include <dht_nonblocking.h>
-#include <LiquidCrystal.h>
 #define DHT_SENSOR_TYPE DHT_TYPE_11
+#define BLU 5
+#define GREEN 6
+#define RED 7
 
 static const int DHT_SENSOR_PIN = 2;
 DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
-LiquidCrystal lcd(7,8,9,10,11,12);
 
 
-/*
- * Initialize the serial port.
- */
-void setup( )
-{
-  lcd.begin(16,2);
+void setup( ) {
+  pinMode(BLU, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(RED, OUTPUT);
   Serial.begin(9600);
 }
 
-
-
-/*
- * Poll for a measurement, keeping the state machine alive.  Returns
- * true if a measurement is available.
- */
-static bool measure_environment( float *temperature, float *humidity )
-{
+static bool measure_environment( float *temperature, float *humidity ) {
   static unsigned long measurement_timestamp = millis( );
 
   /* Measure once every four seconds. */
@@ -40,33 +33,27 @@ static bool measure_environment( float *temperature, float *humidity )
 }
 
 
-
-/*
- * Main program loop.
- */
-void loop( )
-{
+void loop( ) {
   float temperature;
   float humidity;
 
-  /* Measure temperature and humidity.  If the functions returns
-     true, then a measurement is available. */
-  if( measure_environment( &temperature, &humidity ) == true )
-  {
-    lcd.setCursor(0,0);
-    lcd.print("Temp         C  ");
-    lcd.setCursor(6, 0);
-    lcd.print(temperature);
-    
-    lcd.setCursor(0,1);
-    lcd.print("Hu         %  ");
-    lcd.setCursor(4, 1);
-    lcd.print(humidity);
-
+  if( measure_environment( &temperature, &humidity ) == true ) {
 
     Serial.print(temperature);
     Serial.print(":");
     Serial.print(humidity);
     Serial.println();
+
+    digitalWrite(RED, LOW);
+    digitalWrite(BLU, LOW);
+    digitalWrite(GREEN, LOW);
+
+    if(temperature > 25) {
+      digitalWrite(RED, HIGH);   
+    } else if(temperature < 18) {
+      digitalWrite(BLU, HIGH);
+    } else {
+      digitalWrite(GREEN, HIGH);
+    }
   }
 }
